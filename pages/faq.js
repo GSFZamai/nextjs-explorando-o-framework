26fa03d2bf8f9831334d1377c91c997d10751ed9
GSFZamai <gsfzamai@gmail.com>
2023-03-28 21:43:05 -0300

    projeto inicializado

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function Faq() {
+    return (
+        <>
+            <h1>Alura Cases - Página de Faq</h1>
+            <Link href="/">Ir para Home</Link>
+        </>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function Home() {
+    return (
+        <>
+            <h1>Alura Cases - Página inicial</h1>
+            <Link href="faq">Ir para página de faq</Link>
+        </>
+    )
+}
